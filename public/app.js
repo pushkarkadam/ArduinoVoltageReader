5cd1d5c7107f2521bfe7d4fd7842e6f66cb56383
Pushkar Kadam <pushkarkadam17@outlook.com>
2020-01-31 17:19:02 +1100

    Gauge color condition added

--- public/app.js
+++ public/app.js
@@ -8,5 +8,17 @@ socket.on('transmission', function(data){
 });
 
 setInterval(() => {
+    if (value < 25) {
+        document.getElementById("gauge-a").setAttribute("data-color-bar-progress", "rgba(240,47,12,.75)");
+    }
+    else if (value < 50 ) {
+        document.getElementById("gauge-a").setAttribute("data-color-bar-progress", "rgba(240,116,12,.75)");
+    }
+    else if (value < 75) {
+        document.getElementById("gauge-a").setAttribute("data-color-bar-progress", "rgba(232,229,17,.75)");
+    }
+    else {
+        document.getElementById("gauge-a").setAttribute("data-color-bar-progress", "rgba(35,235,12,.75)");
+    }
     document.getElementById("gauge-a").setAttribute("data-value", value);
 }, 1800);
